16fa0a5230e9a05639f16cf487751bd57b481e9a
Svetlana <kovalevasyu@gmail.com>
2023-01-22 19:04:39 +0300

    Задача 2

---
+++ src/com/max/idea/Varchar.java
@@ -0,0 +1,10 @@
+package com.max.idea;
+
+public class Varchar {
+    public static void main(String[] args) {
+        //Задача 2.1
+        double num = 666666.88888;
+        //Задача 2.2
+        byte max = 127;
+    }
+}
