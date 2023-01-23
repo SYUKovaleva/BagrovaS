ab913cb14d4294000b8922c35911770a33e30b9d
Svetlana <kovalevasyu@gmail.com>
2023-01-23 22:51:19 +0300

    Проверочный тест

---
+++ src/com/max/idea/Test.java
@@ -0,0 +1,13 @@
+package com.max.idea;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 10;
+        if (x == 30) {
+            if (y == 10) {
+                System.out.println("Значение х = 20");
+            }
+        }
+    }
+}
