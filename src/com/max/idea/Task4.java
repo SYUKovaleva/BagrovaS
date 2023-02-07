14abd0be7f568033796480bd8b54633671238ee4
Svetlana <kovalevasyu@gmail.com>
2023-02-07 21:30:36 +0300

    Контрольное задание 4

---
+++ src/com/max/idea/Task4.java
@@ -0,0 +1,50 @@
+package com.max.idea;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nОтгадай загадку: 'Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает'\nМожешь взять подсказку, но тогда придётся угадать с одного раза!\nПопытка 1.");
+        String answer;
+        int attempt = 1;
+
+        while (attempt < 4) {
+            System.out.println("Твой ответ:");
+            answer = input.nextLine();
+
+            if (answer.equalsIgnoreCase("Заархивированный вирус")) {
+                System.out.println("Правильно!");
+                break;
+
+            } else if (answer.equalsIgnoreCase("Подсказка") && attempt == 1) {
+                System.out.println("Подсказка: Это точно не ЛУК!");
+                System.out.println("\nПопытка " + attempt + ".");
+                System.out.println("Твой ответ:");
+                answer = input.nextLine();
+
+                if (answer.equalsIgnoreCase("Заархивированный вирус")) {
+                    System.out.println("Правильно!");
+                    break;
+                } else {
+                    System.out.println("Обидно, приходи в другой раз.");
+                    break;
+                }
+
+            } else if (answer.equalsIgnoreCase("Подсказка") && attempt > 1) {
+                System.out.println("Подсказка уже недоступна");
+                System.out.println("\nПопытка " + attempt + ".");
+
+            } else if (attempt < 3) {
+                System.out.println("Подумай еще! \nПопытка " + (attempt + 1) + ".");
+                attempt++;
+            } else if (attempt == 3) {
+                System.out.println("Обидно, приходи в другой раз.");
+                break;
+            }
+        }
+    }
+}
+
